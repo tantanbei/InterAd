59d43d8769c42a5660d396df268e5f1a73170d9f
谈凯德 <tankaide@aiclicash.com>
2017-12-06 15:23:10 +0800

    MOD clean up

--- app/src/main/java/com/aiclk/interad/activity/ActivityInterAd.kt
+++ app/src/main/java/com/aiclk/interad/activity/ActivityInterAd.kt
@@ -2,9 +2,8 @@ package com.aiclk.interad.activity
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.os.Build
 import android.os.Bundle
-import android.view.KeyEvent
-import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.aiclk.interad.R
@@ -21,7 +20,9 @@ class ActivityInterAd : Activity() {
         interAdView = findViewById(R.id.inter_ad)
         interAdView!!.settings.javaScriptEnabled = true
         interAdView!!.settings.domStorageEnabled = true
-        interAdView!!.settings.allowFileAccessFromFileURLs = true
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            interAdView!!.settings.allowFileAccessFromFileURLs = true
+        }
         interAdView!!.webViewClient = WebViewClient()
         interAdView!!.loadUrl("http://cdn.aiclicash.com/game/fuli/fuli.html?iclicashid=7145914")
     }
