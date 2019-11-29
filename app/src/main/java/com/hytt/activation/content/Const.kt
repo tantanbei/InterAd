06d5fdebebcc430ea9a2d3dfadf2ba1230f5e400
tantanbei <tan1115@outlook.com>
2019-11-29 18:59:25 +0800

    mod: update the adslotid

--- app/src/main/java/com/hytt/activation/content/Const.kt
+++ app/src/main/java/com/hytt/activation/content/Const.kt
@@ -1,6 +1,6 @@
 package com.hytt.activation.content
 
 object Const {
-    val AdslotId: String = "5050151"
+    val AdslotId: String = "7270083"
     val Url: String = "https://static.hyrainbow.com/game/turnplate_18/turnplate_18.html?hyid=" + AdslotId + "&channel=app&redpack=1&back=1&dc="
 }
